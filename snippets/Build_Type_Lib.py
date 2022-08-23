d8a31efc04cf6a38ccd2e2c8ca0ec5f43d15f659
pr0xy <proxyjmx@gmail.com>
2022-08-24 05:57:30 +0900

    refactor

--- snippets/Build_Type_Lib.py
+++ snippets/Build_Type_Lib.py
@@ -13,6 +13,138 @@ import requests
 
 BINJA_DIR = binaryninja.bundled_plugin_path().replace("/plugins", "/")
 
+enums = {
+        "flags_t": {
+            "O_RDONLY":     int("00", 8),
+            "O_WRONLY":     int("01", 8),
+            "O_RDWR":       int("02", 8),
+            "O_ACCMODE":    int("03", 8),
+            "O_CREAT":      int("0100", 8),
+            "O_EXCL":       int("0200", 8),
+            "O_NOCTTY":     int("0400", 8),
+            "O_TRUNC":      int("01000", 8),
+            "O_APPEND":     int("02000", 8),
+            "O_NONBLOCK":   int("04000", 8),
+            "O_SYNC":       int("04010000", 8),
+            "O_ASYNC":      int("020000", 8),
+            "O_PATH":       int("010000000", 8),
+            "O_TMPFILE":    int("020000000", 8) | int("0200000", 8),
+            },
+        "fd_t": {
+            "STDIN":    0,
+            "STDOUT":   1,
+            "STDERR":   2,
+            },
+        "whence_t": {
+            "SEEK_SET": 0,
+            "SEEK_CUR": 1,
+            "SEEK_END": 2,
+            "SEEK_DATA": 3,
+            "SEEK_HOLE": 4,
+            },
+        "mmap_prot_t": {
+            "PROT_NONE": 0,
+            "PROT_READ": 1,
+            "PROT_WRITE": 2,
+            "PROT_READ | PROT_WRITE": 3,
+            "PROT_EXEC": 4,
+            "PROT_READ | PROT_EXEC": 5,
+            "PROT_WRITE | PROT_EXEC" : 6,
+            "PROT_READ | PROT_WRITE | PROT_EXEC": 7,
+            },
+        "mmap_flags_t": {
+            "MAP_SHARED": 0x1,
+            "MAP_PRIVATE": 0x2,
+            "MAP_FIXED": 0x10,
+            "MAP_ANONYMOUS": 0x20,
+            },
+        "sig_t": {
+            "SIGHUP"     :  1,
+            "SIGINT"     :  2,
+            "SIGQUIT"    :  3,
+            "SIGILL"     :  4,
+            "SIGTRAP"    :  5,
+            "SIGABRT"    :  6,
+            "SIGBUS"     :  7,
+            "SIGFPE"     :  8,
+            "SIGKILL"    :  9,
+            "SIGUSR1"    : 10,
+            "SIGSEGV"    : 11,
+            "SIGUSR2"    : 12,
+            "SIGPIPE"    : 13,
+            "SIGALRM"    : 14,
+            "SIGTERM"    : 15,
+            "SIGSTKFLT"  : 16,
+            "SIGCHLD"    : 17,
+            "SIGCONT"    : 18,
+            "SIGSTOP"    : 19,
+            "SIGTSTP"    : 20,
+            "SIGTTIN"    : 21,
+            "SIGTTOU"    : 22,
+            "SIGURG"     : 23,
+            "SIGXCPU"    : 24,
+            "SIGXFSZ"    : 25,
+            "SIGVTALRM"  : 26,
+            "SIGPROF"    : 27,
+            "SIGWINCH"   : 28,
+            "SIGIO"      : 29,
+            "SIGPWR"     : 30,
+            "SIGSYS"     : 31,
+            "SIGRTMIN"   : 34,
+            "SIGRTMIN+1" : 35,
+            "SIGRTMIN+2" : 36,
+            "SIGRTMIN+3" : 37,
+            "SIGRTMIN+4" : 38,
+            "SIGRTMIN+5" : 39,
+            "SIGRTMIN+6" : 40,
+            "SIGRTMIN+7" : 41,
+            "SIGRTMIN+8" : 42,
+            "SIGRTMIN+9" : 43,
+            "SIGRTMIN+10": 44,
+            "SIGRTMIN+11": 45,
+            "SIGRTMIN+12": 46,
+            "SIGRTMIN+13": 47,
+            "SIGRTMIN+14": 48,
+            "SIGRTMIN+15": 49,
+            "SIGRTMAX-14": 50,
+            "SIGRTMAX-13": 51,
+            "SIGRTMAX-12": 52,
+            "SIGRTMAX-11": 53,
+            "SIGRTMAX-10": 54,
+            "SIGRTMAX-9" : 55,
+            "SIGRTMAX-8" : 56,
+            "SIGRTMAX-7" : 57,
+            "SIGRTMAX-6" : 58,
+            "SIGRTMAX-5" : 59,
+            "SIGRTMAX-4" : 60,
+            "SIGRTMAX-3" : 61,
+            "SIGRTMAX-2" : 62,
+            "SIGRTMAX-1" : 63,
+            "SIGRTMAX"   : 64,
+            },
+        "shutdown_how_t": {
+            "SHUT_RD"   : 0,
+            "SHUT_WR"   : 1,
+            "SHUT_RDWR" : 2,
+            },
+        "wait_options_t": {
+                "WNOHANG"   : 1,
+                "WUNTRACED" : 2,
+                "WSTOPPED"  : 3,
+                "WEXITED"   : 4,
+                "WEXITED | WNOHANG"   : 5,
+                "WEXITED | WUNTRACED"   : 6,
+                "WEXITED | WUNTRACED | WNOHANG"   : 7,
+                "WCONTINUED": 8,
+                "WCONTINUED | WNOHANG"   : 9,
+                "WCONTINUED | WUNTRACED" : 10,
+                "WCONTINUED | WSTOPPED"  : 11,
+                "WCONTINUED | WEXITED"   : 12,
+                "WCONTINUED | WEXITED | WNOHANG"   : 13,
+                "WCONTINUED | WEXITED | WUNTRACED"   : 14,
+                "WCONTINUED | WEXITED | WUNTRACED | WNOHANG"   : 15,
+                },
+        }
 def create_typelib(fname, name, arch, guid, dependency_name, alternate_names, platform_names, named_objects, named_types):
     typelib = typelibrary.TypeLibrary.new(arch, name)
     typelib.guid = guid
@@ -41,87 +173,69 @@ def build():
 
     tl = typelibrary.TypeLibrary.load_from_file(tl_path)
     arch = tl.arch
-    ignore_list = ["open", "lseek", "mmap"]
+    ignore_list = ["open", "lseek", "mmap", "kill", "shutdown", "waitpid"]
     named_objects = {key:tl.named_objects[key] for key in tl.named_objects if not key in ignore_list }
     named_types = tl.named_types
     
+    for _type in enums:
+        enum_type = EnumerationBuilder.create([], width=4, arch=arch, sign=True)
+        for macro in enums[_type]:
+            enum_type.append(macro, enums[_type][macro])
+        named_types[_type] = enum_type
+
     # open
-    # int open(const char *pathname, int flags)
-    # int open(char *pathname, enum flags_t)
-    enum_type_open_flags = EnumerationBuilder.create([], None, arch=arch)
-    enum_type_open_flags.append("O_RDONLY",    int("00", 8))
-    enum_type_open_flags.append("O_WRONLY",    int("01", 8))
-    enum_type_open_flags.append("O_RDWR",      int("02", 8))
-    enum_type_open_flags.append("O_ACCMODE",   int("03", 8))
-    enum_type_open_flags.append("O_CREAT",     int("0100", 8))
-    enum_type_open_flags.append("O_EXCL",      int("0200", 8))
-    enum_type_open_flags.append("O_NOCTTY",    int("0400", 8))
-    enum_type_open_flags.append("O_TRUNC",     int("01000", 8))
-    enum_type_open_flags.append("O_APPEND",    int("02000", 8))
-    enum_type_open_flags.append("O_NONBLOCK",  int("04000", 8))
-    enum_type_open_flags.append("O_SYNC",      int("04010000", 8))
-    enum_type_open_flags.append("O_ASYNC",     int("020000", 8))
-    enum_type_open_flags.append("O_PATH",      int("010000000", 8))
-    enum_type_open_flags.append("O_TMPFILE",   int("020000000", 8) | int("0200000", 8))
-    named_types["flags_t"] = enum_type_open_flags
-    
+    # before: int open(char *pathname, int flags)
+    # after:  int open(char *pathname, enum flags_t flags)
     ret = Type.int(4)
-    params = [("pathname", Type.pointer(arch, Type.char())),("flags", enum_type_open_flags)]
+    params = [("pathname", Type.pointer(arch, Type.char())), ("flags", named_types["flags_t"])]
     ftype = Type.function(ret, params)
     named_objects["open"] = ftype
     
     # lseek
-    # off_t lseek(int fd, off_t offset, int whence)
-    # off64_t lseek(int32_t fd, int64_t __arg2, int32_t whence)
-    enum_type_fd = EnumerationBuilder.create([], width=4, arch=arch, sign = True)
-    enum_type_fd.append("STDIN", 0)
-    enum_type_fd.append("STDOUT", 1)
-    enum_type_fd.append("STDERR", 2)
-    named_types["fd_t"] = enum_type_fd
-    
-    enum_type_whence = EnumerationBuilder.create([], width=4, arch=arch)
-    enum_type_whence.append("SEEK_SET", 0)
-    enum_type_whence.append("SEEK_CUR", 1)
-    enum_type_whence.append("SEEK_END", 2)
-    enum_type_whence.append("SEEK_DATA", 3)
-    enum_type_whence.append("SEEK_HOLE", 4)
-    named_types["whence_t"] = enum_type_whence
-    
+    # before: off_t lseek(int fd, off_t offset, int whence)
+    # after:  off_t lseek(int fd, off_t offset, enum whence_t whence)
     ret = named_types["off64_t"]
-    params = [("fd", enum_type_fd),("offset", named_types["off64_t"]),("whence",enum_type_whence)]
+    params = [("fd", named_types["fd_t"]), ("offset", named_types["off64_t"]), ("whence", named_types["whence_t"])]
     ftype = Type.function(ret, params)
     named_objects["lseek"] = ftype
     
     # mmap
-    # void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
-    # void *mmap(void* addr, size_t len, int32_t prot, int32_t flags, int32_t fd, off64_t offset)
-    enum_type_mmap_prot = EnumerationBuilder.create([], width=4, arch=arch)
-    enum_type_mmap_prot.append("PROT_NONE", 0)
-    enum_type_mmap_prot.append("PROT_READ", 1)
-    enum_type_mmap_prot.append("PROT_WRITE", 2)
-    enum_type_mmap_prot.append("PROT_READ | PROT_WRITE", 3)
-    enum_type_mmap_prot.append("PROT_EXEC", 4)
-    enum_type_mmap_prot.append("PROT_READ | PROT_EXEC", 5)
-    enum_type_mmap_prot.append("PROT_WRITE | PROT_EXEC" , 6)
-    enum_type_mmap_prot.append("PROT_READ | PROT_WRITE | PROT_EXEC", 7)
-    named_types["mmap_prot_t"] = enum_type_mmap_prot
-    
-    enum_type_mmap_flags = EnumerationBuilder.create([], width=4, arch=arch)
-    enum_type_mmap_flags.append("MAP_SHARED", 0x1)
-    enum_type_mmap_flags.append("MAP_PRIVATE", 0x2)
-    enum_type_mmap_flags.append("MAP_FIXED", 0x10)
-    enum_type_mmap_flags.append("MAP_ANONYMOUS", 0x20)
-    named_types["mmap_flags_t"] = enum_type_mmap_flags
-    
+    # before: void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+    # after : void *mmap(void *addr, size_t length, enum mmap_prot_t prot, enum mmap_flags_t flags, enum fd_t fd, off_t offset);
     ret = Type.pointer(arch, Type.void())
-    params = [("addr", Type.pointer(arch, Type.void())),("length", Type.int(8)), ("prot", enum_type_mmap_prot), ("flags",enum_type_mmap_flags), ("fd", enum_type_fd), ("offset", named_types["off64_t"])]
+    params = [("addr", Type.pointer(arch, Type.void())), ("length", Type.int(8)), ("prot", named_types["mmap_prot_t"]), ("flags", named_types["mmap_flags_t"]), ("fd", named_types["fd_t"]), ("offset", named_types["off64_t"])]
     ftype = Type.function(ret, params)
     named_objects["mmap"] = ftype
     
-    
-    create_typelib(BINJA_DIR + "typelib/x86_64/libc.so.6.bntl", tl.name, arch, tl.guid, tl.dependency_name, tl.alternate_names,tl.platform_names, named_objects, named_types)
+    # kill
+    # before: int kill(pid_t pid, int sig)
+    # after : int kill(pid_t pid, sig_t sig)
+    ret = Type.int(4)
+    params = [("pid", named_types["pid_t"]), ("sig", named_types["sig_t"])]
+    ftype = Type.function(ret, params)
+    named_objects["kill"] = ftype
 
+
+    # shutdown
+    # before: int shutdown(int sockfd, int how)
+    # after:  int shutdown(int sockfd, enum shutdown_how_t how)
+    ret = Type.int(4)
+    params = [("sockfd", Type.int(4)), ("how", named_types["shutdown_how_t"])]
+    ftype = Type.function(ret, params)
+    named_objects["shutdown"] = ftype
+
+    # waitpid
+    # before: pid_t waitpid(pid_t pid, int *wstatus, int options)
+    # after : pid_t waitpid(pid_t pid, int *wstatus, enum wait_options_t options)
+    ret = named_types["pid_t"]
+    params = [("pid", named_types["pid_t"]), ("wstatus", Type.pointer(arch, Type.int(4))), ("options", named_types["wait_options_t"])]
+    ftype = Type.function(ret, params)
+    named_objects["waitpid"] = ftype
+
+    create_typelib(BINJA_DIR + "typelib/x86_64/libc.so.6.bntl", tl.name, arch, tl.guid, tl.dependency_name, tl.alternate_names,tl.platform_names, named_objects, named_types)
     log_info("[Build_Type_Lib(libc.so.6.bntl)] finish")
+
+
 class Builder(BackgroundTaskThread):
     def __init__(self):
         BackgroundTaskThread.__init__(self, "Build Type Lib ...", True)
